b9e4117843ea698f7702958599d27957325e324b
wysnard <vincent.lay77@gmail.com>
2020-06-24 16:18:18 +0200

    url works

--- src/client/containers/app.js
+++ src/client/containers/app.js
@@ -1,16 +1,26 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { register } from '../actions/server'
+import * as cmd from '../../helpers'
 
 console.log(window.location)
 
 const params = window.location.hash
 console.log(params)
 
+const mapKey = {
+  ArrowRight: cmd.RIGHT,
+  ArrowLeft: cmd.LEFT,
+  ArrowUp: cmd.ROTATE,
+  ArrowDown: cmd.DOWN,
+  Space: cmd.FALL,
+  Escape: cmd.PAUSE
+}
+
 let token = null
 let nb_player = null
-let player_name = 'toto'
-let room_name = 'tata'
+let player_name = params.slice(params.indexOf('[')+1, -1)
+let room_name = params.substr(1, params.indexOf('[')-1)
 
 const App = ({socket}) => {
   if (!socket)
@@ -20,15 +30,8 @@ const App = ({socket}) => {
   console.log('socket:',socket)
   return (
     <div>
-      {/* <span>{socket}</span> */}
       <button onClick={() => {
-        socket.emit('register', register(room_name, player_name));
-      }}>register</button>
-      <button onClick={() => {
-        socket.emit(`room#${room_name}`, "right");
-      }}>room</button>
-      <button onClick={() => {
-        console.log("token player:", token, nb_player)
+        console.log("token nb_player:", token, nb_player)
       }}>token player</button>
       <script> {maincode(socket)}</script>
     </div>
@@ -36,9 +39,21 @@ const App = ({socket}) => {
 }
 
 function maincode(socket){
+  console.log("player name:", player_name)
+  console.log("room name:", room_name)
+  socket.emit('register', register(room_name, player_name))
   console.log("mainSocket socket:", socket)
   socket.on('register', (msg) => ({token, nb_player} = msg))
-  socket.on(`room#${room_name}`, (msg) => console.log(msg))
+  socket.on(`room#${room_name}`, (msg) => console.log(`[REPONSE] room#${room_name}`, msg))
+
+  // Ecoute les key pressed et les envoies au back
+  document.addEventListener('keyup', (key) => {
+    if (key.code in mapKey) {
+      const command = mapKey[key.code]
+      console.log(`[SEND] room#${room_name}`, {token, command})
+      socket.emit(`room#${room_name}`, {token, command})
+    }
+  })
 }
 
 const mapStateToProps = (state) => {
--- src/client/reducers/socket.js
+++ src/client/reducers/socket.js
@@ -3,7 +3,7 @@ import { SOCKET_REQUEST, SOCKET_SUCCESS, SOCKET_FAILURE } from '../actions/socke
 export const socket_reducer = (state = {} , action) => {
   switch(action.type){
     case SOCKET_SUCCESS:
-      return { socket: action.socket }
+      return Object.assign({}, state, { socket: action.socket })
     default: 
       return state
   }
