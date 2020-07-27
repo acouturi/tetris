043fb4259d08bf3239f4162c4c463a1906e21494
wysnard <vincent.lay77@gmail.com>
2020-07-27 13:32:55 +0200

    test react

--- src/client/actions/order.js
+++ src/client/actions/order.js
@@ -1,43 +1,18 @@
-const ORDER = 'ORDER'
+import * as cmd from '../../helpers'
 
-export const right = (event) => {
-    return {
-        type: 'ArrowRight',
-        event: event
-    }
+// key.code
+const mapKey = {
+  ArrowRight: cmd.RIGHT,
+  ArrowLeft: cmd.LEFT,
+  ArrowUp: cmd.ROTATE,
+  ArrowDown: cmd.DOWN,
+  Space: cmd.FALL,
+  Escape: cmd.PAUSE
 }
 
-export const left = (event) => {
+export const order = (event) => {
     return {
-        type: 'ArrowLeft',
-        event: event
-    }
-}
-
-export const rotate = (event) => {
-    return {
-        type: 'ArrowUp',
-        event: event
-    }
-}
-
-export const down = (event) => {
-    return {
-        type: 'ArrowDown',
-        event: event
-    }
-}
-
-export const fall = (event) => {
-    return {
-        type: 'Space',
-        event: event
-    }
-}
-
-export const pause = (event) => {
-    return {
-        type: 'Escape',
+        type: mapKey[event.code],
         event: event
     }
 }
